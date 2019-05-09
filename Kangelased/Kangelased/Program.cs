3ab4c452d64bf031e3546fe5768772c6af796dc4
Kristoferlatt <kristoferlatt09@gmail.com>
2019-05-09 12:33:48 +0300

    Testi terviku töötamist

--- Kangelased/Kangelased/Program.cs
+++ Kangelased/Kangelased/Program.cs
@@ -30,6 +30,12 @@ namespace Kangelased
         
         static void Main(string[] args)
         {
+            LaeKangelased("kangelased.txt");
+            foreach (Kangelane tegelane in Kangelased)
+            {
+                Console.WriteLine(tegelane);
+                Console.WriteLine("Ohus on 1000 inimest. Päästa " + tegelane.Päästa(1000));
+            }
 
         }
     }
