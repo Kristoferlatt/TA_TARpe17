debb5e1a0aa2cb3c88e2945d3b03531b40a82a41
Kristoferlatt <kristoferlatt09@gmail.com>
2019-05-15 14:13:47 +0300

    Loo klassi Kangelane konstruktor.

--- Kangelased/Kangelased/Kangelane.cs
+++ Kangelased/Kangelased/Kangelane.cs
@@ -9,17 +9,24 @@ namespace Kangelased
     class Kangelane
     {
         private string _Nimi;
+        private string _SuperNimi;
+        private string _V6ime;
         private string _Asukoht;
 
-        public Kangelane(string Nimi, string Asukoht)
+
+        public Kangelane(string Nimi, string Asukoht, string SuperNimi, string V6ime)
         {
             _Nimi = Nimi;
             _Asukoht = Asukoht;
+            _SuperNimi = SuperNimi;
+            _V6ime = V6ime;
 
         }
 
         public string Nimi { get => _Nimi; set => _Nimi = value; }
         public string Asukoht { get => _Asukoht; set => _Asukoht = value; }
+        public string SuperNimi { get => _SuperNimi; set => _SuperNimi = value; }
+        public string V6ime { get => _V6ime; set => _V6ime = value; }
         public virtual int Päästa(int Ohustatuid)
         {
             return (int)Math.Round(Ohustatuid * .95);
